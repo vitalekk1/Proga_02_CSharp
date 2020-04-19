3bc6ff99d75c2c76db1ec5d4e558bf568aa0fbe1
vitalekk1 <vitalikkakanov55@gmail.com>
2020-04-19 22:05:49 +0500

    Добавьте файлы проекта.

---
+++ Proga_02_CSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proga_02_CSharp
+{
+    class Program
+    {
+        private static Int32 s = 0, x;
+        static void Main(string[] args)
+        {
+            x = Convert.ToInt32(Console.ReadLine());
+            while (x != 0)
+            {
+                s = s + (x % 10);
+                x = x / 10;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
